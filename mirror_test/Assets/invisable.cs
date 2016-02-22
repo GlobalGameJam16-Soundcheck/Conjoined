63bca30bba53aa45365d5fc6a7648450d1814e89
Akiva42 <akiva@cmu.edu>
2016-02-21 20:41:55 -0500

    basic project from unity example

---
+++ mirror_test/Assets/invisable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class invisable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color (0,0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
