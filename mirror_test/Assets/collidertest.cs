f9201ba09edc19c793b04054fbe31026e47f41a8
Akiva42 <akiva@cmu.edu>
2016-02-27 23:26:29 -0500

    tree demo

---
+++ mirror_test/Assets/collidertest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collidertest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Contllider other)
+    {
+        print("Still colliding with trigger object " + other.name);
+    }
+}
---
+++ tempTree/Assets/scripts/playerControler.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControler : MonoBehaviour {
+
+    Rigidbody2D myRig;
+    Collider2D myCollider;
+    SpriteRenderer mySprite;
+    bool canJump;
+    public float jumpPower = 40.0f;
+    public float runSpeed = 2.0f;
+    public float maxRunSpeed = 10.0f;
+    float k_GroundedRadius = 0.3f;
+    Transform m_GroundCheck;
+    public float hFirction = 0.02f;
+
+    // Use this for initialization
+    void Awake () {
+        myRig = gameObject.GetComponent<Rigidbody2D>();
+        myCollider = gameObject.GetComponent<Collider2D>();
+        mySprite = gameObject.GetComponent<SpriteRenderer>();
+        m_GroundCheck = transform.Find("GroundCheck");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(myRig.velocity.x > 0)
+        {
+            mySprite.flipX = true;
+        }
+        if(myRig.velocity.x < 0)
+        {
+            mySprite.flipX = false;
+        }
+        myRig.velocity = new Vector2(myRig.velocity.x * hFirction, myRig.velocity.y);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, LayerMask.NameToLayer("Platform"));
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                canJump = true;
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            if (canJump)
+            {
+                myRig.velocity = new Vector2(myRig.velocity.x, jumpPower);
+                canJump = false;
+            }
+        }
+        if (Input.GetKey("left"))
+        {
+            if (myRig.velocity.x > -maxRunSpeed)
+            {
+                myRig.velocity = new Vector2(myRig.velocity.x - runSpeed, myRig.velocity.y);
+            }
+        }
+        if (Input.GetKey("right"))
+        {
+            if (myRig.velocity.x < maxRunSpeed)
+            {
+                myRig.velocity = new Vector2(myRig.velocity.x + runSpeed, myRig.velocity.y);
+            }
+        }
+    }
+}
