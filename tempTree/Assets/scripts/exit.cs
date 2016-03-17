71c72a9dec6d93adf8d74aaa91c1360e6055a978
Akiva42 <akiva@cmu.edu>
2016-03-17 15:37:19 -0400

    new textures and level exits

---
+++ tempTree/Assets/scripts/exit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "player")
+        {
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                int i = Application.loadedLevel;
+                Application.LoadLevel(i + 1);
+            }
+        }
+    }
+}
